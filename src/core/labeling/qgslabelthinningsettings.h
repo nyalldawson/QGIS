nyalldawson/Quantum-GIS

[FEATURE] Add labeling setting to prevent duplicate labels within xx mm of each other
Improves the appearance of labels in situations like road networks,
where things like dual carriageways, service roads, etc can result
in many labels of the exact same text appearing close to each other.

src/core/labeling/qgslabelthinningsettings.h
@@ -18,6 +18,8 @@
 
 #include "qgis_core.h"
 #include "qgis_sip.h"
+#include "qgsmapunitscale.h"
+#include "qgsunittypes.h"
 
 class QgsPropertyCollection;
 class QgsExpressionContext;
@@ -78,6 +80,62 @@ class CORE_EXPORT QgsLabelThinningSettings
     void setMinimumFeatureSize( double size ) { mMinFeatureSize = size; }
 
     /**
+     * Returns the minimum distance to labels with duplicate text. Units are specified through minimumDistanceToDuplicateUnits().
+     * \see setMinimumDistanceToDuplicate()
+     * \see minimumDistanceToDuplicateUnits()
+     */
+    double minimumDistanceToDuplicate() const { return mMinDistanceToDuplicate; }
+
+    /**
+     * Sets the minimum \a distance to labels with duplicate text. Units are specified through setMinimumLengthUnit().
+     * \see minimumDistanceToDuplicate()
+     * \see setMinimumDistanceToDuplicateUnits()
+     */
+    void setMinimumDistanceToDuplicate( double length ) { mMinDistanceToDuplicate = length; }
+
+    /**
+     * Sets the \a unit for the minimum distance to labels with duplicate text.
+     * \see minimumDistanceToDuplicateUnits()
+     * \see setMinimumDistanceToDuplicate()
+    */
+    void setMinimumDistanceToDuplicateUnits( QgsUnitTypes::RenderUnit unit ) { mMinDistanceToDuplicateUnits = unit; }
+
+    /**
+     * Returns the units for the minimum distance to labels with duplicate text.
+     * \see setMinimumDistanceToDuplicateUnits()
+     * \see minimumDistanceToDuplicate()
+    */
+    QgsUnitTypes::RenderUnit minimumDistanceToDuplicateUnits() const { return mMinDistanceToDuplicateUnits; }
+
+    /**
+     * Sets the map unit \a scale for the minimum distance to labels with duplicate text.
+     * \see minimumDistanceToDuplicateMapUnitScale()
+     * \see minimumDistanceToDuplicateUnits()
+     * \see setMinimumDistanceToDuplicate()
+     */
+    void setMinimumDistanceToDuplicateMapUnitScale( const QgsMapUnitScale &scale ) { mMinDistanceToDuplicateScale = scale; }
+
+    /**
+     * Returns the map unit scale for the minimum distance to labels with duplicate text.
+     * \see setMinimumDistanceToDuplicateMapUnitScale()
+     * \see minimumDistanceToDuplicateUnits()
+     * \see minimumDistanceToDuplicate()
+     */
+    const QgsMapUnitScale &minimumDistanceToDuplicateMapUnitScale() const { return mMinDistanceToDuplicateScale; }
+
+    /**
+     * Returns whether duplicate label removal is permitted for this layer.
+     * \see setAllowDuplicateRemoval()
+     */
+    bool allowDuplicateRemoval() const { return mAllowDuplicateRemoval; }
+
+    /**
+     * Sets whether duplicate label removal is permitted for this layer.
+     * \see setAllowDuplicateRemoval()
+     */
+    void setAllowDuplicateRemoval( bool allow ) { mAllowDuplicateRemoval = allow; }
+
+    /**
      * Updates the thinning settings to respect any data defined properties
      * set within the specified \a properties collection.
      */
@@ -88,6 +146,47 @@ class CORE_EXPORT QgsLabelThinningSettings
     bool mLimitNumLabels = false;
     int mMaxNumLabels = 2000;
     double mMinFeatureSize = 0;
+    bool mAllowDuplicateRemoval = false;
+    double mMinDistanceToDuplicate = 0;
+    QgsUnitTypes::RenderUnit mMinDistanceToDuplicateUnits = QgsUnitTypes::RenderMillimeters;
+    QgsMapUnitScale mMinDistanceToDuplicateScale;
 };
+
+#ifndef SIP_RUN
+
+/**
+ * \ingroup core
+ * \class QgsLabelFeatureThinningSettings
+ *
+ * Contains settings related to how the label engine removes candidate label positions and reduces the number
+ * of displayed labels for one particular label feature.
+ *
+ * \since QGIS 3.12
+ */
+class CORE_EXPORT QgsLabelFeatureThinningSettings
+{
+  public:
+
+    /**
+     * Returns the minimum distance (in label units) between labels for this
+     * feature and other labels with the same label text.
+     * \see setNoRepeatDistance()
+     */
+    double noRepeatDistance() const { return mNoRepeatDistance; }
+
+    /**
+     * Sets the minimum \a distance (in label units) between labels for this
+     * feature and other labels with the same label text.
+     * \see noRepeatDistance()
+     */
+    void setNoRepeatDistance( double distance ) { mNoRepeatDistance = distance; }
+
+  private:
+
+    double mNoRepeatDistance = 0;
+
+};
+
+#endif
 
 #endif // QGSLABELTHINNINGSETTINGS_H

src/gui/labeling/qgslabelremoveduplicatesettingswidget.h
@@ -1,0 +1,69 @@
+/***************************************************************************
+    qgslabelremoveduplicatessettingswidget.h
+    ----------------------
+    begin                : December 2019
+    copyright            : (C) 2019 by Nyall Dawson
+    email                : nyall dot dawson at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSLABELREMOVEDUPLICATESSETTINGSWIDGET_H
+#define QGSLABELREMOVEDUPLICATESSETTINGSWIDGET_H
+
+#include "ui_qgslabelremoveduplicatesettingswidgetbase.h"
+#include "qgslabelsettingswidgetbase.h"
+#include "qgspallabeling.h"
+#include "qgis_gui.h"
+#include "qgis_sip.h"
+
+/**
+ * \ingroup gui
+ * \class QgsLabelRemoveDuplicatesSettingsWidget
+ * A widget for customising label duplicate removal settings.
+ * \since QGIS 3.12
+ */
+class GUI_EXPORT QgsLabelRemoveDuplicatesSettingsWidget : public QgsLabelSettingsWidgetBase, private Ui::QgsLabelRemoveDuplicateSettingsWidgetBase
+{
+    Q_OBJECT
+
+  public:
+
+    /**
+     * Constructor for QgsLabelRemoveDuplicatesSettingsWidget.
+     * \param parent parent widget
+     * \param vl associated vector layer
+     */
+    QgsLabelRemoveDuplicatesSettingsWidget( QWidget *parent SIP_TRANSFERTHIS = nullptr, QgsVectorLayer *vl = nullptr );
+
+    /**
+     * Sets the thinning \a settings to show in the widget.
+     *
+     * \see settings()
+     */
+    void setSettings( const QgsLabelThinningSettings &settings );
+
+    /**
+     * Returns the thinning settings defined by the widget.
+     *
+     * \see setSettings()
+     */
+    QgsLabelThinningSettings settings() const;
+
+    void setGeometryType( QgsWkbTypes::GeometryType type ) override;
+
+    void updateDataDefinedProperties( QgsPropertyCollection &properties ) override;
+
+  private:
+
+    bool mBlockSignals = false;
+    mutable QgsLabelThinningSettings mSettings;
+
+};
+
+#endif // QGSLABELREMOVEDUPLICATESSETTINGSWIDGET_H

nyalldawson/QGIS

src/core/labeling/qgslabelthinningsettings.h
@@ -18,6 +18,7 @@
 
 #include "qgis_core.h"
 #include "qgis_sip.h"
+#include "qgsmapunitscale.h"
 
 class QgsPropertyCollection;
 class QgsExpressionContext;
@@ -78,6 +79,78 @@ class CORE_EXPORT QgsLabelThinningSettings
     void setMinimumFeatureSize( double size ) { mMinFeatureSize = size; }
 
     /**
+     * Returns the minimum distance to labels with duplicate text. Units are specified through minimumDistanceToDuplicateUnits().
+     * \see setMinimumDistanceToDuplicate()
+     * \see minimumDistanceToDuplicateUnits()
+     *
+     * \since QGIS 3.20
+     */
+    double minimumDistanceToDuplicate() const { return mMinDistanceToDuplicate; }
+
+    /**
+     * Sets the minimum \a distance to labels with duplicate text. Units are specified through setMinimumLengthUnit().
+     * \see minimumDistanceToDuplicate()
+     * \see setMinimumDistanceToDuplicateUnits()
+     *
+     * \since QGIS 3.20
+     */
+    void setMinimumDistanceToDuplicate( double length ) { mMinDistanceToDuplicate = length; }
+
+    /**
+     * Sets the \a unit for the minimum distance to labels with duplicate text.
+     * \see minimumDistanceToDuplicateUnits()
+     * \see setMinimumDistanceToDuplicate()
+     *
+     * \since QGIS 3.20
+    */
+    void setMinimumDistanceToDuplicateUnits( Qgis::RenderUnit unit ) { mMinDistanceToDuplicateUnits = unit; }
+
+    /**
+     * Returns the units for the minimum distance to labels with duplicate text.
+     * \see setMinimumDistanceToDuplicateUnits()
+     * \see minimumDistanceToDuplicate()
+     *
+     * \since QGIS 3.20
+    */
+    Qgis::RenderUnit minimumDistanceToDuplicateUnits() const { return mMinDistanceToDuplicateUnits; }
+
+    /**
+     * Sets the map unit \a scale for the minimum distance to labels with duplicate text.
+     * \see minimumDistanceToDuplicateMapUnitScale()
+     * \see minimumDistanceToDuplicateUnits()
+     * \see setMinimumDistanceToDuplicate()
+     *
+     * \since QGIS 3.20
+     */
+    void setMinimumDistanceToDuplicateMapUnitScale( const QgsMapUnitScale &scale ) { mMinDistanceToDuplicateScale = scale; }
+
+    /**
+     * Returns the map unit scale for the minimum distance to labels with duplicate text.
+     * \see setMinimumDistanceToDuplicateMapUnitScale()
+     * \see minimumDistanceToDuplicateUnits()
+     * \see minimumDistanceToDuplicate()
+     *
+     * \since QGIS 3.20
+     */
+    const QgsMapUnitScale &minimumDistanceToDuplicateMapUnitScale() const { return mMinDistanceToDuplicateScale; }
+
+    /**
+     * Returns whether duplicate label removal is permitted for this layer.
+     * \see setAllowDuplicateRemoval()
+     *
+     * \since QGIS 3.20
+     */
+    bool allowDuplicateRemoval() const { return mAllowDuplicateRemoval; }
+
+    /**
+     * Sets whether duplicate label removal is permitted for this layer.
+     * \see setAllowDuplicateRemoval()
+     *
+     * \since QGIS 3.20
+     */
+    void setAllowDuplicateRemoval( bool allow ) { mAllowDuplicateRemoval = allow; }
+
+    /**
      * Updates the thinning settings to respect any data defined properties
      * set within the specified \a properties collection.
      */
@@ -88,6 +161,47 @@ class CORE_EXPORT QgsLabelThinningSettings
     bool mLimitNumLabels = false;
     int mMaxNumLabels = 2000;
     double mMinFeatureSize = 0;
+    bool mAllowDuplicateRemoval = false;
+    double mMinDistanceToDuplicate = 0;
+    Qgis::RenderUnit mMinDistanceToDuplicateUnits = Qgis::RenderUnit::Millimeters;
+    QgsMapUnitScale mMinDistanceToDuplicateScale;
 };
+
+#ifndef SIP_RUN
+
+/**
+ * \ingroup core
+ * \class QgsLabelFeatureThinningSettings
+ *
+ * Contains settings related to how the label engine removes candidate label positions and reduces the number
+ * of displayed labels for one particular label feature.
+ *
+ * \since QGIS 3.20
+ */
+class CORE_EXPORT QgsLabelFeatureThinningSettings
+{
+  public:
+
+    /**
+     * Returns the minimum distance (in label units) between labels for this
+     * feature and other labels with the same label text.
+     * \see setNoRepeatDistance()
+     */
+    double noRepeatDistance() const { return mNoRepeatDistance; }
+
+    /**
+     * Sets the minimum \a distance (in label units) between labels for this
+     * feature and other labels with the same label text.
+     * \see noRepeatDistance()
+     */
+    void setNoRepeatDistance( double distance ) { mNoRepeatDistance = distance; }
+
+  private:
+
+    double mNoRepeatDistance = 0;
+
+};
+
+#endif
 
 #endif // QGSLABELTHINNINGSETTINGS_H

src/gui/labeling/qgslabelremoveduplicatesettingswidget.h
@@ -1,0 +1,69 @@
+/***************************************************************************
+    qgslabelremoveduplicatessettingswidget.h
+    ----------------------
+    begin                : December 2019
+    copyright            : (C) 2019 by Nyall Dawson
+    email                : nyall dot dawson at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSLABELREMOVEDUPLICATESSETTINGSWIDGET_H
+#define QGSLABELREMOVEDUPLICATESSETTINGSWIDGET_H
+
+#include "ui_qgslabelremoveduplicatesettingswidgetbase.h"
+#include "qgslabelsettingswidgetbase.h"
+#include "qgspallabeling.h"
+#include "qgis_gui.h"
+#include "qgis_sip.h"
+
+/**
+ * \ingroup gui
+ * \class QgsLabelRemoveDuplicatesSettingsWidget
+ * A widget for customising label duplicate removal settings.
+ * \since QGIS 3.12
+ */
+class GUI_EXPORT QgsLabelRemoveDuplicatesSettingsWidget : public QgsLabelSettingsWidgetBase, private Ui::QgsLabelRemoveDuplicateSettingsWidgetBase
+{
+    Q_OBJECT
+
+  public:
+
+    /**
+     * Constructor for QgsLabelRemoveDuplicatesSettingsWidget.
+     * \param parent parent widget
+     * \param vl associated vector layer
+     */
+    QgsLabelRemoveDuplicatesSettingsWidget( QWidget *parent SIP_TRANSFERTHIS = nullptr, QgsVectorLayer *vl = nullptr );
+
+    /**
+     * Sets the thinning \a settings to show in the widget.
+     *
+     * \see settings()
+     */
+    void setSettings( const QgsLabelThinningSettings &settings );
+
+    /**
+     * Returns the thinning settings defined by the widget.
+     *
+     * \see setSettings()
+     */
+    QgsLabelThinningSettings settings() const;
+
+    void setGeometryType( Qgis::GeometryType type ) override;
+
+    void updateDataDefinedProperties( QgsPropertyCollection &properties ) override;
+
+  private:
+
+    bool mBlockSignals = false;
+    mutable QgsLabelThinningSettings mSettings;
+
+};
+
+#endif // QGSLABELREMOVEDUPLICATESSETTINGSWIDGET_H