188271816947add8e436fd4f55af9b133643d05a
Nyall Dawson <nyall.dawson@gmail.com>
2023-11-20 12:07:51 +1000

    Fix incorrect collapse of angled lines

--- src/core/symbology/qgslinedistancerenderer.cpp
+++ src/core/symbology/qgslinedistancerenderer.cpp
@@ -400,6 +400,9 @@ void QgsLineDistanceRenderer::stopRender( QgsRenderContext &context )
                              segmentData.x1, segmentData.y1,
                              segmentData.x2, segmentData.y2, ptX2, ptY2, 0 );
 
+        //if ( dist1 > tolerance * tolerance || dist2 > tolerance * tolerance )
+        // continue;
+
         if ( qgsDoubleNear( ptX1, ptX2 ) && qgsDoubleNear( ptY1, ptY2 ) )
         {
           // lines are parallel which come close, but don't actually overlap at all
@@ -409,6 +412,7 @@ void QgsLineDistanceRenderer::stopRender( QgsRenderContext &context )
 
         double minDistX;
         double minDistY;
+#if 0
         // take the start/end of the segment, and find the nearest point on the candidate to those
         const double dist3 = QgsGeometryUtils::sqrDistToLine( segmentData.x1, segmentData.y1,
                              candidateSegment.x1, candidateSegment.y1,
@@ -416,18 +420,32 @@ void QgsLineDistanceRenderer::stopRender( QgsRenderContext &context )
         const double dist4 = QgsGeometryUtils::sqrDistToLine( segmentData.x2, segmentData.y2,
                              candidateSegment.x1, candidateSegment.y1,
                              candidateSegment.x2, candidateSegment.y2, minDistX, minDistY, 0 );
-
-        const double minDistance = std::sqrt( std::min( std::min( std::min( dist1, dist2 ), dist3 ), dist4 ) );
-        if ( minDistance > tolerance )
+        const double minDistance = std::min( std::min( std::min( dist1, dist2 ), dist3 ), dist4 );
+        if ( minDistance > tolerance * tolerance )
         {
           // lines are parallel, but minimum distance between them is larger than the tolerance
           continue;
         }
 
+#endif
+        // project closest points from segment back onto candidate
+        const double dist3 = QgsGeometryUtils::sqrDistToLine( ptX1, ptY1,
+                             candidateSegment.x1, candidateSegment.y1,
+                             candidateSegment.x2, candidateSegment.y2, minDistX, minDistY, 0 );
+        const double dist4 = QgsGeometryUtils::sqrDistToLine( ptX2, ptY2,
+                             candidateSegment.x1, candidateSegment.y1,
+                             candidateSegment.x2, candidateSegment.y2, minDistX, minDistY, 0 );
+        if ( std::max( dist3, dist4 ) > tolerance * tolerance )
+          continue;
+
         // calculate split points - the distance along the segment at which the overlap starts/ends
         const double split1 = std::sqrt( std::pow( segmentData.x1 - ptX1, 2 ) + std::pow( segmentData.y1 - ptY1, 2 ) );
-        splitPoints.insert( split1 );
         const double split2 = std::sqrt( std::pow( segmentData.x1 - ptX2, 2 ) + std::pow( segmentData.y1 - ptY2, 2 ) );
+
+
+
+
+        splitPoints.insert( split1 );
         splitPoints.insert( split2 );
 
         outSegments.insert( candidate );
