2ee1187247ec597fbd4dcd5cc3420a6f1822ef63
Nyall Dawson <nyall.dawson@gmail.com>
2026-08-22 15:14:10 +1000

    const correct

--- src/gui/processing/qgsprocessingwidgetwrapper.h
+++ src/gui/processing/qgsprocessingwidgetwrapper.h
@@ -244,7 +244,7 @@ class GUI_EXPORT QgsProcessingParameterWidgetContext
      * \see registerProcessingContextGenerator()
      * \since QGIS 4.0
      */
-    QgsProcessingContextGenerator *processingContextGenerator();
+    const QgsProcessingContextGenerator *processingContextGenerator() const;
 
     /**
      * Returns the associated model designer dialog, if applicable.
