630ec442985abf3c12d0c6d301749bc05068b09c
Nyall Dawson <nyall.dawson@gmail.com>
2026-08-22 15:43:18 +1000

    Don't expose classes to Python which aren't needed

--- src/gui/processing/models/qgsmodelcomponentgraphicitem.h
+++ src/gui/processing/models/qgsmodelcomponentgraphicitem.h
@@ -499,10 +499,11 @@ class GUI_EXPORT QgsModelParameterGraphicItem : public QgsModelComponentGraphicI
     QPicture mPicture;
 };
 
+#ifndef SIP_RUN
 /**
  * \ingroup gui
  * \brief A graphic item representing a child algorithm in the model designer.
- * \warning Not stable API
+ * \warning Not available in Python bindings
  * \since QGIS 3.14
  */
 class GUI_EXPORT QgsModelChildAlgorithmGraphicItem : public QgsModelComponentGraphicItem
@@ -673,7 +674,7 @@ class GUI_EXPORT QgsModelChildAlgorithmGraphicItem : public QgsModelComponentGra
     double mProgress = -1;
     bool mIsValid = true;
 };
-
+#endif
 
 /**
  * \ingroup gui
@@ -713,7 +714,7 @@ class GUI_EXPORT QgsModelOutputGraphicItem : public QgsModelComponentGraphicItem
     QPicture mPicture;
 };
 
-
+#ifndef SIP_RUN
 /**
  * \ingroup gui
  * \brief A graphic item representing a model comment in the model designer.
@@ -761,7 +762,7 @@ class GUI_EXPORT QgsModelCommentGraphicItem : public QgsModelComponentGraphicIte
     std::unique_ptr<QgsProcessingModelComponent> mParentComponent;
     QPointer<QgsModelComponentGraphicItem> mParentItem;
 };
-
+#endif
 
 /**
  * \ingroup gui
