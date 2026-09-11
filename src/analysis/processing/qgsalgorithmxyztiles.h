f8a143dcb7b5a730218c5a4457bb953d7760a0eb
Nyall Dawson <nyall.dawson@gmail.com>
2026-09-11 18:00:42 +1000

    [feature] Add webp as image format option for generate XYZ tiles (directory)
    Allows creating tiles in the webp format

--- src/analysis/processing/qgsalgorithmxyztiles.h
+++ src/analysis/processing/qgsalgorithmxyztiles.h
@@ -100,6 +100,7 @@ class QgsXyzTilesBaseAlgorithm : public QgsProcessingAlgorithm
 
     void startJobs();
     virtual void processMetaTile( QgsMapRendererSequentialJob *job ) = 0;
+    virtual bool supportsWebP() const { return true; }
 
     QgsRectangle mExtent;
     QColor mBackgroundColor;
@@ -173,6 +174,7 @@ class QgsXyzTilesMbtilesAlgorithm : public QgsXyzTilesBaseAlgorithm
     QVariantMap processAlgorithm( const QVariantMap &parameters, QgsProcessingContext &context, QgsProcessingFeedback *feedback ) override;
 
     void processMetaTile( QgsMapRendererSequentialJob *job ) override;
+    bool supportsWebP() const override { return false; }
 
   private:
     std::unique_ptr<QgsMbTiles> mMbtilesWriter;
